beaba4d7deb3f87f0d72453c7adf2115c1a30e3e
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-07-27 01:15:22 +0300

    set good

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'dist', // Set this if your app is served from a subpath
   plugins: [react()],
+   base: '/',
+  build: {
+    rollupOptions: {
+      // Additional Rollup options
+    }
+  }
 });
