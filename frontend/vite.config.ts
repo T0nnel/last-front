289779b71ed09f3a4cf1527a250ff0c7bcee4277
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-07-27 01:09:13 +0300

    woi

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: 'dist', // Set this if your app is served from a subpath
   plugins: [react()],
-})
+});
